8c75039e8d92e5336cb2104c1c19c2d8ac84d292
Antoine HAMON <antoine.hamon22@gmail.com>
2019-01-14 12:43:36 +0100

    changes

--- scrapper.py
+++ scrapper.py
@@ -76,7 +76,9 @@ class browser:
         try:
             self.waitForElement('#bnc-compte-href').click()
             self.waitForElement("//a[contains(., '" + account + "')]",By.XPATH).click()
-                  
+            
+            print('XPATH OK')
+            
             date_pattern = re.compile('[0-9]{2}/[0-9]{2}')
 
             out=[]
