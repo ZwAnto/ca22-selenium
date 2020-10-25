7efc9686aff35b24c122cb0471a6c3af41a75b17
antoine.hamon.lincoln <antoine.hamon.lincoln@servier.com>
2020-10-25 16:47:27 +0100

    major update

--- main.py
+++ main.py
@@ -1,130 +1,124 @@
 
-# ARGUMENT PARSER #############################################
-
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("conf", help="Path to config.ini") 
-args = parser.parse_args()
-
-# MODULES #####################################################
-
-import numpy as np
-import pymysql
-from scrapper import browser, md5_hash
-from mail import sendMail
-import pandas as pd
-import configparser
-import sys
+import logging
 import os
 import re
+import sys
 
-# CONFIGURATION FILE ##########################################
+import plac
+import pyaml
+import pymysql
 
-sys.stdout.write('Reading config.ini... ')
-sys.stdout.flush()
-config = configparser.ConfigParser()
-config.read(args.conf)
-print('\033[92mOK\033[0m')
+from scrapper import browser, md5_hash
 
-sql_config = {
-    'user':         config['SQL']['USERNAME'],
-    'password':     config['SQL']['PASSWORD'],
-    'host':         config['SQL']['HOST'],
-    'database':     config['SQL']['DATABASE'],
-    'port':         int(config['SQL']['PORT'])           
-    }
+logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s',level=logging.INFO)
 
-# BROWSER INITIALISATION ######################################
+def main(config_file='config.yml'):
 
-sys.stdout.write('Browser initialization... ')
-sys.stdout.flush()
-chrome = browser(config['CHROME']['DRIVER'])
-print('\033[92mOK\033[0m')
+    if not os.path.isfile(config_file):
+        logging.error('File %s does not exist.', config_file)
+        exit()
 
-# LOGIN #######################################################
+    # CONFIGURATION FILE ##########################################
 
-sys.stdout.write('Login... ')
-sys.stdout.flush()
-chrome.connect(config['LOGIN']['ACCOUNT'],
-               config['LOGIN']['PASSWORD'])
-print('\033[92mOK\033[0m')
-
-# RETRIEVE OPERATIONS #########################################
-
-try:
-    sql = pymysql.connect(**sql_config)
-except pymysql.OperationalError as err:
-    print('Erreur de connexion')
-else:
-    cursor = sql.cursor()
-    
-    # Retrieveing last 10 op
-    cursor.execute('SELECT date_record,date_value,date_desc,type,description,debit,credit,date FROM (SELECT * FROM ' + config['SQL']['TABLE'] + ' ORDER BY id DESC LIMIT 10) as t1')
-    result = np.asarray(cursor.fetchall())
-    
-    # Computing MD5 hash
-    md5 = md5_hash(result) 
-    
-    # Closing SQL connection
-    cursor.close()
-    sql.close()
-
-    # Scraping operations
-    print('Retrieving operation... ')
-    operations, new_md5 = chrome.retrieve(config['BANK']['ACCOUNT'],md5)
-    chrome.quit()
-    
-    sendMail(len(operations),config['MAIL']['USER'],config['MAIL']['PASSWORD'])
-    
-    if new_md5 == md5:
-        print('Already up to date')
+    try:
+        config = pyaml.yaml.load(open(config_file), Loader=pyaml.yaml.loader.BaseLoader)
+    except Exception:
+        logging.exception('Error during reading of %s', config_file)
+        exit()
     else:
-        print('')
-        print('%i operations added' % (len(operations)))
+        logging.info('Configuration file sucessfully imported.')
+
+    # BROWSER INITIALISATION ######################################
+
+    try:
+        chrome = browser(config['chromedriver'])
+    except Exception:
+        logging.exception('Error during browser initialisation.')
+        exit()
+    else:
+        logging.info('Browser initialized sucessfully.')
+
+    # LOGIN #######################################################
+
+    try:
+        chrome.connect(**config['login'])
+    except Exception:
+        logging.exception('Error during login.')
+        exit()
+    else:
+        logging.info('Login sucessfull.')
+
+    # RETRIEVE OPERATIONS #########################################
+
+    try:
+        sql_config = {
+            'user':         config['sql']['username'],
+            'password':     config['sql']['password'],
+            'host':         config['sql']['host'],
+            'database':     config['sql']['database'],
+            'port':         int(config['sql']['port'])           
+        }
+        sql = pymysql.connect(**sql_config)
+    except:
+        logging.exception('Error during sql connection initialisation.')
+        chrome.quit()
+        exit()
+    else:
+        logging.info('SQL connection established.')
         
+    # HASH #########################################
+
+    try:
+        cursor = sql.cursor()
+        
+        # Retrieveing last 10 op
+        _ = cursor.execute('SELECT date_record,date_value,date_desc,type,description,debit,credit,date FROM (SELECT * FROM scraping ORDER BY id DESC LIMIT 10) as t1')
+        result = np.asarray(cursor.fetchall())
+        
+        # Computing MD5 hash
+        md5 = md5_hash(result) 
+        logging.debug('Hash of last 10 sql entries: %s', md5)
+        # Closing sql connection
+        cursor.close()
+    except:
+        logging.exception('Error querying last 10 entries in scrapping table.')
+        chrome.quit()
+        sql.close()
+        exit()
+
+    # RETRIEVING
+
+    try:
+        operations, new_md5 = chrome.retrieve(config['bank_account'], md5)
+        chrome.quit()
+    except:
+        logging.exception('Error during new operation retrieval.')
+        chrome.quit()
+        exit()
+    else:
+        logging.info('%i operations retrieved.', len(operations))
+
+
+
+    if new_md5 == md5:
+        logging.info('Database up-to-date.')
+    else:
         try:
-            sql = pymysql.connect(**sql_config)
-        except pymysql.OperationalError as err:
-            print('Erreur de connexion')
-        else:
             cursor = sql.cursor()
             
-            # Pushing new op to database
-            sys.stdout.write('Pushing to database... ')
-            sys.stdout.flush()
-
-            cursor.executemany('INSERT INTO ' + config['SQL']['TABLE'] + ' (date_record,date_value,date_desc,type,description,debit,credit, date) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)', list(reversed(operations)))
+            cursor.executemany('INSERT INTO scraping (date_record,date_value,date_desc,type,description,debit,credit, date) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)', list(reversed(operations)))
             sql.commit()
             
-            print('\033[92mOK\033[0m')
-            
-            # Computing missing year
-            sys.stdout.write('Computing missing years... ')
-            sys.stdout.flush()
-            
-            cursor.execute('SELECT max(id),max(year) from ref_year')
-            year = np.asarray(cursor.fetchall())
-
-            cursor.execute('SELECT * from scraping where id >= ' + str(year[0][0]))
-            scraping = pd.DataFrame(np.asarray(cursor.fetchall()))
-            scraping.columns = np.asarray(cursor.description)[:,0]
-
-            i = 'date_record'
-
-            year_ = scraping[i].str.split('/').str[1]
-            year_ = (year_.shift(1) > year_).cumsum() + year[0][1]
-
-            vals = [[int(i),j] for j,i in zip(year_[1:],scraping['id'][1:].values)]
-
-            # Pushing years in ref_year            
-            if len(vals):
-                cursor.executemany("insert into ref_year(id, year) values (%s, %s)", vals )
-                sql.commit()
-    
-            print('\033[92mOK\033[0m')
-    
-            # Closing sql connection
             cursor.close()
+        except:
+            logging.exception('Error during SQL INSERT query.')
             sql.close()
-            print('\033[92mOK\033[0m')
+            exit()
+        else:
+            logging.info('Database up-to-date.')
+        
+    sql.close()
+    exit()
+
+if __name__ == '__main__':
+    import plac; plac.call(main)
--- scrapper.py
+++ scrapper.py
@@ -8,19 +8,46 @@ import pickle
 import hashlib
 import time
 
+MONTHS = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+DATE_PATTERN = re.compile('[0-9]{2}/[0-9]{2}')
+
 def md5_hash(arr):
     if type(arr) == np.ndarray:
         arr = arr.tolist()
     return(hashlib.md5(pickle.dumps([[str(i).encode('utf-8') for i in j] for j in arr])).hexdigest())
 
-class HistoryReachedError(Exception):
-    def __init__(self, message, errors):
+def parse_date(date):
+    date = re.split('[ ,]', date)
+    month = [i+1 for i,m in enumerate(MONTHS) if m == date[0]][0]
+    return date[1].zfill(2) + '/' + str(month).zfill(2), date[3] + '-' + str(month).zfill(2) + '-' + date[1].zfill(2) 
 
-        # Call the base class constructor with the parameters it needs
-        super().__init__(message)
+def parse_montant(montant):
+    if montant[0] == '-':
+        debit = re.sub(',','.',re.sub('[-+] ?([0-9, ]*).*','\\1',montant)).strip()
+        debit = str(float(re.sub(' ','',debit)))
+        credit = str(float(0))
+    else:
+        credit = re.sub(',','.',re.sub('[-+] ?([0-9, ]*).*','\\1',montant)).strip()
+        credit = str(float(re.sub(' ','',credit)))
+        debit = str(float(0))
 
-        # Now for your custom code...
-        errors = errors
+    return debit, credit
+
+
+def load_more(driver):
+    if driver.browser.find_element_by_css_selector('.js-Operations-more').is_displayed():
+        driver.browser.execute_script("$('.js-Operations-more').click()")
+        time.sleep(.5)
+
+        while True:
+            try:
+                driver.browser.find_element_by_css_selector('.Operations-more--loading')
+                time.sleep(0.5)
+            except:
+                break
+        return True
+    else:
+        return False
 
 class browser:
     
@@ -47,8 +74,10 @@ class browser:
         self.browser = webdriver.Chrome(executable_path=self.chromedriver_path, options=self.option, desired_capabilities= self.caps)
         self.browser.set_window_size(1920, 1080)
 
-  
-    def connect(self,account,passwd):
+    def quit(self):
+        self.browser.quit()
+
+    def connect(self,account,password):
         self.browser.get("https://www.credit-agricole.fr/ca-cotesdarmor/particulier/acceder-a-mes-comptes.html")
         time.sleep(0.5)
         self.browser.find_element_by_css_selector('input[name="CCPTE"]').send_keys(account)
@@ -62,7 +91,7 @@ class browser:
         for i in pass_btn:
             map[i.find_element_by_css_selector('div').text] = i
 
-        for i in passwd:
+        for i in password:
             map[i].click()
         
         self.browser.find_element_by_css_selector('#validation').click()
@@ -73,49 +102,32 @@ class browser:
     def retrieve(self,account,last_md5=None):
         try:
             
-            months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
-            self.browser.get('https://www.credit-agricole.fr/ca-cotesdarmor/particulier/operations/synthese/detail-comptes.html?idx=1&famillecode=1#!/')
-
-            for _ in range(10):
-                self.browser.execute_script("$('.js-Operations-more').click()")
-                time.sleep(5)
-            
-            ops = self.browser.find_elements_by_css_selector('#bloc-operations li')
-
-            def parse_date(date):
-                date = re.split('[ ,]', date)
-                month = [i+1 for i,m in enumerate(months) if m == date[0]][0]
-                return date[1].zfill(2) + '/' + str(month).zfill(2), date[3] + '-' + str(month).zfill(2) + '-' + date[1].zfill(2) 
- 
-            def parse_montant(montant):
-                if montant[0] == '-':
-                    debit = re.sub(',','.',re.sub('[-+] ?([0-9, ]*).*','\\1',montant)).strip()
-                    debit = str(float(re.sub(' ','',debit)))
-                    credit = str(float(0))
-                else:
-                    credit = re.sub(',','.',re.sub('[-+] ?([0-9, ]*).*','\\1',montant)).strip()
-                    credit = str(float(re.sub(' ','',credit)))
-                    debit = str(float(0))
-
-                return debit, credit
-
-
-            date_pattern = re.compile('[0-9]{2}/[0-9]{2}')
+            self.browser.get('https://www.credit-agricole.fr/ca-cotesdarmor/particulier/operations/synthese/detail-comptes.html?idx={}&famillecode=1#!/'.format(account))
 
             out = []
-            for op in ops:
-                try:
+            history_reached = False
+
+            while not history_reached:
+
+                if not load_more(self):
+                    new_md5 = md5_hash(out[:10]) 
+                    break
+
+                ops = self.browser.find_elements_by_css_selector('#bloc-operations li')
+
+                for op in ops:
+
                     date_op, full_date = parse_date(op.find_element_by_css_selector("#dateOperation").get_attribute('aria-label'))
                     date_val, _ = parse_date(op.find_element_by_css_selector("#dateValeur").get_attribute('aria-label'))
                     op_type = op.find_element_by_css_selector("div .Operation-type").text
                     op_name = op.find_element_by_css_selector("div .Operation-name").text
                     debit, credit = parse_montant(op.find_element_by_css_selector("#montant").text)
 
-                    date_desc = date_pattern.findall(op_name)
+                    date_desc = DATE_PATTERN.findall(op_name)
                     date_desc = date_op if not len(date_desc) else date_desc[0]
                     
-                    op_name = date_pattern.sub('', op_name)
+                    op_name = DATE_PATTERN.sub('', op_name)
 
                     for i in op.find_elements_by_css_selector('.Operation-list .Operation-main div'):
                         op_name = op_name + ' ' + i.get_attribute("textContent")
@@ -130,20 +142,15 @@ class browser:
                     if last_md5 is not None:
                         md5 = md5_hash(out[-10:]) 
                         if md5 == last_md5:
-                            raise(HistoryReachedError('a','a'))
-    
-                except NoSuchElementException:
-                    break
-                except HistoryReachedError:
-                    new_md5 = md5_hash(out[:10])
-                    out = out[:-10]
-                    break
-                else:
-                    new_md5 = md5_hash(out[:10]) 
+                            out = out[:-10]
+                            new_md5 = md5_hash(out[:10])
+                            history_reached = True
+                            break
+
             return((out,new_md5))
+
         except Exception as e: 
             print(str(e))
             quit()
 
-    def quit(self):
-        self.browser.quit()
+
