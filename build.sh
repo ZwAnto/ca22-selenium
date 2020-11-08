d82f75a50d3d32c89bc87f16e0e861f92d5f1786
antoine.hamon.lincoln <antoine.hamon.lincoln@servier.com>
2020-11-08 19:12:20 +0100

    changes

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-docker build -t ca22 .
-
-docker build -t ca22_selenium ~/github/ca22-backend/
+docker build -t ca22_selenium ~/github/ca22-selenium/
 docker tag ca22_selenium localhost:5000/ca22_selenium
 docker push localhost:5000/ca22_selenium
