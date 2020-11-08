0fd74141482de9ac453f23c768c0d70d864449cb
antoine.hamon.lincoln <antoine.hamon.lincoln@servier.com>
2020-11-08 21:02:19 +0100

    changes

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+export PUSHED_APP_KEY=$1 
+export PUSHED_APP_SECRET=$2
+export CA_LOGIN=$3
+export CA_PASSWORD=$4
+export CA_ACCOUNT=$5
+
+printenv
+
 docker build -t ca22_selenium ~/github/ca22-selenium/
 docker tag ca22_selenium localhost:5000/ca22_selenium
 docker push localhost:5000/ca22_selenium
