b54b929c6106a9f65c8055c2a5a619bff7c65f3b
zwanto <antoine.hamon@protonmail.com>
2021-04-29 22:22:26 +0200

    change stack deploy

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ docker build -t ca22_selenium ~/github/ca22-selenium/
 docker tag ca22_selenium localhost:5000/ca22_selenium
 docker push localhost:5000/ca22_selenium
 docker stack rm ca22_selenium
-docker stack deploy -c ~/github/ca22-selenium/ca22_selenium.yml ca22_selenium
+docker stack deploy --prune --with-registry-auth --resolve-image=always -c ~/github/ca22-selenium/ca22_selenium.yml ca22_selenium
