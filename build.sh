d27719cf6bc49403b19120897c360fc97dc4e847
Antoine HAMON <antoine.hamon@protonmail.com>
2021-06-14 22:10:12 +0200

    Change registry

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ export CA_ACCOUNT=$5
 printenv
 
 docker build -t ca22_selenium ~/github/ca22-selenium/
-docker tag ca22_selenium localhost:5000/ca22_selenium
-docker push localhost:5000/ca22_selenium
+docker tag ca22_selenium registry.ahamon.ovh/ca22_selenium
+docker push registry.ahamon.ovh/ca22_selenium
 docker stack rm ca22_selenium
 docker stack deploy --prune --with-registry-auth --resolve-image=always -c ~/github/ca22-selenium/ca22_selenium.yml ca22_selenium
